43f570eda9ec64e62e3d569b16d94397fa1db6e0
Диана Волкова <diansvolkava@gmail.com>
2024-07-30 08:17:31 +0300

    Update Zad9.java

--- Zad9.java
+++ Zad9.java
@@ -1,16 +1,3 @@
 public class Zad9 {
-    public static boolean isLeapYear(int year) {
-        if (year % 4 != 0) {
-            return false;
-        } else if (year % 100 == 0) {
-            return year % 400 == 0;
-        } else {
-            return true;
-        }
-    }
-    public static void main(String[] args) {
-        int year = 2024;
-        boolean isLeap = isLeapYear(year);
-        System.out.println(year + " is a leap year: " + isLeap);
-    }
+    return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
 }
