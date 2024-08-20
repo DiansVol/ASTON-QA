47284e8e5bf90a16c3c7872a9f63240a7f9366e4
Диана Волкова <diansvolkava@gmail.com>
2024-08-20 10:03:31 +0300

    Add files via upload

---
+++ Dog.java
@@ -0,0 +1,15 @@
+class Dog extends Animal {
+    public Dog(String name) {
+        super(name);
+    }
+
+    @Override
+    public void run(int distance) {
+        System.out.println(name + " пробежал " + distance + " м.");
+    }
+
+    @Override
+    public void swim(int distance) {
+        System.out.println(name + " проплыл " + distance + " м.");
+    }
+}
